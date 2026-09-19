c13a066ce1c972deea7eac57112d4991baef8b5c
meerajoy676 <meerajoy676@gmail.com>
2026-09-19 08:06:06 +0530

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class RockPaperScissors {
+    static String playRound(String playerMove, String computerMove) {
+        if (playerMove.equals(computerMove))
+            return "Draw";
+
+        if ((playerMove.equals("Rock") && computerMove.equals("Scissors")) ||
+            (playerMove.equals("Paper") && computerMove.equals("Rock")) ||
+            (playerMove.equals("Scissors") && computerMove.equals("Paper")))
+            return "Player Wins";
+
+        return "Computer Wins";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        String[] moves = {"Rock", "Paper", "Scissors"};
+        String[] player = new String[5];
+        String[] computer = new String[5];
+        String[] result = new String[5];
+
+        int wins = 0, losses = 0, draws = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter your move: ");
+            player[i] = sc.next();
+
+            computer[i] = moves[r.nextInt(3)];
+            result[i] = playRound(player[i], computer[i]);
+
+            if (result[i].equals("Player Wins"))
+                wins++;
+            else if (result[i].equals("Computer Wins"))
+                losses++;
+            else
+                draws++;
+        }
+
+        System.out.println("\nRound\tPlayer\t\tComputer\tResult");
+
+        for (int i = 0; i < 5; i++)
+            System.out.println((i + 1) + "\t" + player[i] + "\t\t" +
+                    computer[i] + "\t\t" + result[i]);
+
+        double winPercentage = wins * 100.0 / 5;
+
+        System.out.println("\nWins: " + wins);
+        System.out.println("Losses: " + losses);
+        System.out.println("Draws: " + draws);
+        System.out.printf("Win Percentage: %.1f%%", winPercentage);
+    }
+}
