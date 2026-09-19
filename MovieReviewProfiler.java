82ca4d45ede94b967d9a9c803961f3679bcad521
meerajoy676 <meerajoy676@gmail.com>
2026-09-19 08:16:53 +0530

    Add files via upload

---
+++ MovieReviewProfiler.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class MovieReviewProfiler {
+    static void classifyWordLengths(String review) {
+        String[] words = review.split("\\s+");
+
+        int shortWords = 0;
+        int mediumWords = 0;
+        int longWords = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            int length = words[i].length();
+
+            if (length <= 4)
+                shortWords++;
+            else if (length <= 8)
+                mediumWords++;
+            else
+                longWords++;
+        }
+
+        System.out.println("Short: " + shortWords);
+        System.out.println("Medium: " + mediumWords);
+        System.out.println("Long: " + longWords);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter movie review: ");
+        String review = sc.nextLine();
+
+        classifyWordLengths(review);
+    }
+}
